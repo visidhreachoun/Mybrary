672088c044c2d2f6a1a0f46c1ff19515550da3da
MACHOME <machome@korisu.local>
2026-02-19 12:52:34 +0700

    Books Create/Index/New

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const router = express.Router();
-
-router.get("/", (req, res) => {
-  res.render("index");
+const Book = require("../models/books");
+router.get("/", async (req, res) => {
+  let books;
+  try {
+    books = await Book.find().sort({ createdAt: "desc" }).limit(10).exec();
+  } catch {
+    books = [];
+  }
+  res.render("index", { books: books });
 });
 
 module.exports = router;
